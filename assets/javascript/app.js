516600debb7cdf9dc43234d2bdcf3a6f3e037209
Thor Nolan <tnolan5764@gmail.com>
2019-03-18 18:34:36 -0700

    Updated hover color for navbar and added a sidenav for medium and smaller screen sizes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,13 @@
-// Initiate parallax and scrollspy functionality
+// Initiate parallax, scrollspy, and sidenav functionality
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    //$('.sidenav').sidenav();
+    $('.button-collapse').sideNav({
+        menuWidth: 175,
+        closeOnClick: true,
+        edge: 'right'
+    });
     $('input#input_text, textarea#textarea2').characterCounter();
     // this adds the click to toggle class to my floating action button on smaller screen sizes where the hover effect doesn't implement properly
     $( window ).resize(function() {
