4c55953f527273094bc3548b7cac28be385f7ba4
Thor Nolan <tnolan5764@gmail.com>
2019-03-25 18:57:50 -0700

    Added copy to clipboard functionality to my email floating action button using clipboard.js.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
-// Initiate parallax, scrollspy, and sidenav functionality
+// Initiate parallax, scrollspy, sidenav, and tooltip functionality
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    $('.tooltipped').tooltip();
+
     //$('.sidenav').sidenav();
     $('.button-collapse').sideNav({
         menuWidth: 175,
@@ -23,6 +25,28 @@ $(document).ready(function(){
     //       $(".topNav").removeClass("navbar-scrolled");
     //     }
     // });
+
+    // Copy email address to clipboard by clicking on floating action button email icon	
+    var clipboard = new ClipboardJS('#email');
+
+	clipboard.on('success', function(e) {
+        var anchorElement = $('#email');
+	    anchorElement.attr('data-tooltip', 'Copied to your clipboard ✅');
+        anchorElement.addClass('success');
+        anchorElement.tooltip();
+
+	    // Reset after a timeout
+	    anchorElement.mouseleave(function() {
+
+		    setTimeout( function(){
+			    anchorElement.attr('data-tooltip', 'Click to copy my email address to your clipboard!');
+                anchorElement.removeClass('success');
+                anchorElement.tooltip();
+			}, 300);
+		});
+
+	    e.clearSelection();
+	});
 });
 
 
