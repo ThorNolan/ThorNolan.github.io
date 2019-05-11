31c9a59889de5ee6b53cbba4b6964b80e3cf4b5b
Thor Nolan <tnolan5764@gmail.com>
2019-05-10 17:55:53 -0700

    Update skills section significantly, different format with better organization

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,168 +1,165 @@
 // Initiate parallax, scrollspy, and sidenav functionality
-$(document).ready(function(){
-    $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
-    $('.button-collapse').sideNav({
-        menuWidth: 180,
-        closeOnClick: true,
-        edge: 'left'
+$(document).ready(function () {
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+  $('.button-collapse').sideNav({
+    menuWidth: 180,
+    closeOnClick: true,
+    edge: 'left'
+  });
+  $('input#input_text, textarea#textarea2').characterCounter();
+
+  // this adds the click to toggle class to my floating action button on smaller screen sizes where the hover effect can't implement properly
+  $(window).resize(function () {
+    if ($(window).width() <= 640) $(".actionButton").addClass("click-to-toggle");
+  });
+
+  // navbar opacity feature
+  // $(window).scroll( function() {
+  //     // apply css classes based on scroll positioning
+  //     if ($(".topNav").offset().top > 100) {
+  //       $(".topNav").addClass("navbar-scrolled");
+  //     } else {
+  //       $(".topNav").removeClass("navbar-scrolled");
+  //     }
+  // });
+
+  // Copy email address to clipboard by clicking on floating action button email icon	
+  var clipboard = new ClipboardJS('#email');
+
+  // store my email button and tooltip text in variables
+  var anchorElement = $('#email');
+  var tooltipText = $('.material-tooltip span');
+
+  // triggers on successful copy to clipboard and alters text within the tooltip to indicate success
+  clipboard.on('success', function (e) {
+
+    tooltipText.html('Copied to your clipboard ✅');
+
+    // Reset tooltip message after a timeout
+    anchorElement.mouseleave(function () {
+
+      setTimeout(function () {
+        tooltipText.html('Copy my email to your clipboard!');
+      }, 3000);
     });
-    $('input#input_text, textarea#textarea2').characterCounter();
 
-    // this adds the click to toggle class to my floating action button on smaller screen sizes where the hover effect can't implement properly
-    $( window ).resize(function() {
-        if($(window).width() <= 640) $(".actionButton").addClass("click-to-toggle");
-    });
+    e.clearSelection();
+  });
 
-    // navbar opacity feature
-    // $(window).scroll( function() {
-    //     // apply css classes based on scroll positioning
-    //     if ($(".topNav").offset().top > 100) {
-    //       $(".topNav").addClass("navbar-scrolled");
-    //     } else {
-    //       $(".topNav").removeClass("navbar-scrolled");
-    //     }
-    // });
-
-    // Copy email address to clipboard by clicking on floating action button email icon	
-    var clipboard = new ClipboardJS('#email');
-
-    // store my email button and tooltip text in variables
-    var anchorElement = $('#email');
-    var tooltipText = $('.material-tooltip span');
-
-    // triggers on successful copy to clipboard and alters text within the tooltip to indicate success
-	clipboard.on('success', function(e) {
-
-        tooltipText.html('Copied to your clipboard ✅');
-
-	    // Reset tooltip message after a timeout
-	    anchorElement.mouseleave(function() {
-
-		    setTimeout(function(){
-                tooltipText.html('Copy my email to your clipboard!');
-			}, 3000);
-		});
-
-	    e.clearSelection();
-    });
-    
-    particlesJS("particles-js", {
-        "particles": {
-          "number": {
-            "value": 180,
-            "density": {
-              "enable": true,
-              "value_area": 4008.530152163807
-            }
-          },
-          "color": {
-            "value": "#0ac8d4"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0,
-              "color": "#0ac8d4"
-            },
-            "polygon": {
-              "nb_sides": 4
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.4734885849793636,
-            "random": true,
-            "anim": {
-              "enable": true,
-              "speed": 0.7192807192807193,
-              "opacity_min": 0.5674325674325674,
-              "sync": true
-            }
-          },
-          "size": {
-            "value": 2.5,
-            "random": true,
-            "anim": {
-              "enable": true,
-              "speed": 7.192807192807193,
-              "size_min": 0.7992007992007992,
-              "sync": false
-            }
-          },
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 200,
+        "density": {
+          "enable": true,
+          "value_area": 4008.530152163807
+        }
+      },
+      "color": {
+        "value": "#0ac8d4"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#0ac8d4"
+        },
+        "polygon": {
+          "nb_sides": 4
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.4734885849793636,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 0.7192807192807193,
+          "opacity_min": 0.5674325674325674,
+          "sync": true
+        }
+      },
+      "size": {
+        "value": 2.5,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 7.192807192807193,
+          "size_min": 0.7992007992007992,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 64.13648243462092,
+        "color": "#0ac8d4",
+        "opacity": 0.33933348590187723,
+        "width": 1.4204657549380908
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "bounce",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 2446.3576890600452,
+          "rotateY": 236.7442924896818
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true,
+        "onresize": {
+          "enable": true,
+          "density_auto": true,
+          "density_area": 400 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+        }
+      },
+      "modes": {
+        "grab": {
+          "distance": 263.73626373626377,
           "line_linked": {
-            "enable": true,
-            "distance": 64.13648243462092,
-            "color": "#0ac8d4",
-            "opacity": 0.33933348590187723,
-            "width": 1.4204657549380908
-          },
-          "move": {
-            "enable": true,
-            "speed": 1,
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "bounce",
-            "bounce": false,
-            "attract": {
-              "enable": true,
-              "rotateX": 2446.3576890600452,
-              "rotateY": 236.7442924896818
-            }
+            "opacity": 1
           }
         },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "grab"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "push"
-            },
-            "resize": true,
-            "onresize": {
-              "enable": true,
-              "density_auto": true,
-              "density_area": 400 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
-            }
-          },
-          "modes": {
-            "grab": {
-              "distance": 263.73626373626377,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 121.81158184520176,
-              "size": 27.972027972027973,
-              "duration": 2,
-              "opacity": 8,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 0,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
+        "bubble": {
+          "distance": 121.81158184520176,
+          "size": 27.972027972027973,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
         },
-        "retina_detect": true
-      });
+        "repulse": {
+          "distance": 0,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
 });
-
-
-     
