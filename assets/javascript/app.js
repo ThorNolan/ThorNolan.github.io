f7413698cc290438afa3c585fa6f9f51630552d5
Thor Nolan <tnolan5764@gmail.com>
2019-06-11 00:40:17 -0700

    Add preloader in case of small load time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,20 +9,15 @@ $(document).ready(function () {
   });
   $('input#input_text, textarea#textarea2').characterCounter();
 
+  // preloader will fade out once dom is loaded
+  $(window).on("load",function(){
+    $(".loader-container").fadeOut("slow");
+  });
+
   // this adds the click to toggle class to my floating action button on smaller screen sizes where the hover effect can't implement properly
   $(window).resize(function () {
     if ($(window).width() <= 640) $(".actionButton").addClass("click-to-toggle");
   });
-
-  // navbar opacity feature
-  // $(window).scroll( function() {
-  //     // apply css classes based on scroll positioning
-  //     if ($(".topNav").offset().top > 100) {
-  //       $(".topNav").addClass("navbar-scrolled");
-  //     } else {
-  //       $(".topNav").removeClass("navbar-scrolled");
-  //     }
-  // });
 
   // Copy email address to clipboard by clicking on floating action button email icon	
   var clipboard = new ClipboardJS('#email');
