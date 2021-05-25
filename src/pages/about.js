614d6050c243739db5422ee91f28a900ca5b3615
Thor Nolan <tnolan5764@gmail.com>
2021-05-24 23:50:55 -0700

    Fix display issue on small screens with entrance animations

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,23 +11,27 @@ const About = () => (
     <section className="section page-content" id="about-me">
       <div className="container">
         <div className="about-row">
-          <div id="profile-pic" data-sal="slide-up" data-sal-delay="250" data-sal-duration="450" data-sal-easing="ease">
+          <div id="profile-pic">
             <img 
               src={profilePic}
               loading="eager"
               alt="Thor Nolan smiling, sitting in a house made of driftwood"
+              data-sal="slide-up"
+              data-sal-delay="200"
+              data-sal-duration="450"
+              data-sal-easing="ease"
             /> 
           </div>
 
           <div className="about-text">
-            <p id="intro" data-sal="slide-up" data-sal-delay="300" data-sal-duration="450" data-sal-easing="ease">I'm Thor, a developer and web designer based in
+            <p id="intro" data-sal="slide-up" data-sal-delay="200" data-sal-duration="400" data-sal-easing="ease">I'm Thor, a developer and web designer based in
               the Bay Area, CA. I focus on front end development, and appreciate any opportunity to hone my craft and bring my creativity to interesting projects.
               I like to collaborate with people to come up with strategies and solutions that work.</p>
             <br />
-            <p data-sal="slide-up" data-sal-delay="350" data-sal-duration="450" data-sal-easing="ease">When I'm not coding, I like to climb at my local climbing gym, and I love to travel whenever I can. I'm interested in technology, politics, philosophy, teaching, 
+            <p data-sal="slide-up" data-sal-delay="250" data-sal-duration="400" data-sal-easing="ease">When I'm not coding, I like to climb at my local climbing gym, and I love to travel whenever I can. I'm interested in technology, politics, philosophy, teaching, 
               and everything about this beautiful planet we live on. I also built my desktop PC that I use for playing games and designing things in Illustrator</p>
             <br />
-            <p data-sal="slide-up" data-sal-delay="400" data-sal-duration="450" data-sal-easing="ease">I'm sometimes available for freelance work, so if you have a project you'd
+            <p data-sal="slide-up" data-sal-delay="300" data-sal-duration="400" data-sal-easing="ease">I'm sometimes available for freelance work, so if you have a project you'd
               like to work together on <a href="mailto:tnolan5764@gmail.com" id="email" name="tnolan5764@gmail.com">send me an email</a> and we can chat about it 
               <span role="img" aria-label="wave" id="wave">🌊</span> 
             </p>
