d6f6dd027a805433fa1a962727affaab76670c46
Thor Nolan <tnolan5764@gmail.com>
2021-05-24 23:37:10 -0700

    Fix mobile usability issue with about page, navigation

--- src/components/WaveSkills/waveSkills.js
+++ src/components/WaveSkills/waveSkills.js
@@ -27,7 +27,7 @@ const WaveSkills = () => {
             <li><i className="material-icons tiny">keyboard_arrow_right</i> Express server </li>
             <li><i className="material-icons tiny">keyboard_arrow_right</i> Working with RESTful API's </li>
             <li><i className="material-icons tiny">keyboard_arrow_right</i> ESlint, smoke/unit testing</li>
-            <li><i className="material-icons tiny">keyboard_arrow_right</i> Agile methodologies </li>
+            <li id="down"><i className="material-icons tiny">keyboard_arrow_right</i> Agile methodologies </li>
           </ul>
         </div>
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,9 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Seo from "../components/Wrapper/seo.js"
 import WaveSkills from "../components/WaveSkills/waveSkills.js"
 import "./about.scss"
+import profilePic from "../images/profile-pic.jpg";
 
 const About = () => (
   <>
@@ -12,13 +12,11 @@ const About = () => (
       <div className="container">
         <div className="about-row">
           <div id="profile-pic" data-sal="slide-up" data-sal-delay="250" data-sal-duration="450" data-sal-easing="ease">
-            <StaticImage 
+            <img 
+              src={profilePic}
               loading="eager"
-              src="../images/profile-pic.jpg"
               alt="Thor Nolan smiling, sitting in a house made of driftwood"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-            />
+            /> 
           </div>
 
           <div className="about-text">
@@ -30,32 +28,23 @@ const About = () => (
               and everything about this beautiful planet we live on. I also built my desktop PC that I use for playing games and designing things in Illustrator</p>
             <br />
             <p data-sal="slide-up" data-sal-delay="400" data-sal-duration="450" data-sal-easing="ease">I'm sometimes available for freelance work, so if you have a project you'd
-              like to work together on <a href="mailto:tnolan5764@gmail.com" id="email" name="tnolan5764@gmail.com">send me an email</a> and we can chat about it <span role="img" aria-label="wave" id="wave">🌊</span></p>
-            <div className="resume-container" data-sal="slide-up" data-sal-delay="450" data-sal-duration="450" data-sal-easing="ease">
-              <a href="assets/images/resume.pdf" target="_blank">
-                <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
-                  <rect className="border-animate" height="60" width="320" />
-                </svg>
-
-                <span className="outlined-text">→ my resume</span>
-              </a>
-            </div>
+              like to work together on <a href="mailto:tnolan5764@gmail.com" id="email" name="tnolan5764@gmail.com">send me an email</a> and we can chat about it 
+              <span role="img" aria-label="wave" id="wave">🌊</span> 
+            </p>
           </div>
-
         </div>
       </div>
       <div>
-        <div data-sal="slide-up" data-sal-delay="1000" data-sal-duration="600" data-sal-easing="ease" className="down-button-container">
+        <div data-sal="slide-up" data-sal-delay="600" data-sal-duration="500" data-sal-easing="ease" className="down-button-container">
           <a
             href="#down"
             aria-label="WHAT I BRING TO THE TABLE"
           >
-            <h4>WHAT I BRING TO THE TABLE</h4>
+            <h4><i>WHAT I BRING TO THE TABLE</i></h4>
             <i className="fa fa-chevron-down fa-lg" />
           </a>
         </div>
         <WaveSkills />
-        <div id="down"></div>
       </div>
     </section>
   </>
