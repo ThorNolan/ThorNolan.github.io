0a28c67e07942cf06f6a09eb99f8a39411c6d5a2
Thor Nolan <tnolan5764@gmail.com>
2021-07-14 14:13:37 -0700

    Add initial component and code for custom cursor element

---
+++ src/components/CustomCursor/customCursor.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import "./customCursor.scss"
+
+const CustomCursor = () => {
+
+  return (
+    <div className="cursor"></div>
+    <div className="cursor"></div>
+  )
+}
+
+export default CustomCursor
